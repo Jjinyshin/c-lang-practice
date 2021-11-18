f757615ae9c13832d9c282cbc72a15c4298f7ac1
Jjinyshin <cspfshin@gmail.com>
2021-11-18 22:05:35 +0900

    9.1 코드 review
    이렇게 커밋하는게 맞나 싶긴  함.  
    중간에 폴더위치를 변경해서 생긴 것 같은 change들이 있음

---
+++ Section9/9.2.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+// 콘솔에 나의 뽀짝한 명함 출력하기
+
+#define WIDTH 25
+#define NAME "Jin-Young Shin"
+#define ADDRESS "Anyang, Korea"
+
+void print_multiple_chars(char c, int n_stars, bool print_newline)
+{
+	for (int i = 0; i < n_stars; ++i)
+		printf("%c", c); // putchar(c)도 가능
+
+	if (print_newline)
+		printf("\n");
+}
+
+void print_centered_str(char str[])
+{
+	int n_blanks = 0;
+	n_blanks = (WIDTH - strlen(str)) / 2;
+	print_multiple_chars(' ', n_blanks, false);
+	printf("%s\n", str);
+}
+
+int main()
+{
+	print_multiple_chars('*', WIDTH, true);
+
+	print_centered_str(NAME);
+	print_centered_str(ADDRESS);
+	print_centered_str("How are you?");
+
+	print_multiple_chars('*', WIDTH, true);
+	return 0;
+}
