54d770a276c6dcf964fe7949e7b4cd8e998c3463
Jjinyshin <cspfshin@gmail.com>
2021-11-22 23:59:28 +0900

    변수의 영역과 지역변수

--- Section9/9.4/9.4.c
+++ Section9/9.4/9.4.c
@@ -1,8 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+
+// 전역변수와 지역변수(복습)
+
+int int_max(int i, int j)
+{
+	int m;
+	m = i > j ? i : j;
+	return m;
+}
 
 int main()
 {
-	printf("Hello, World!");
+	m = 123;
 
 	return 0;
 }
