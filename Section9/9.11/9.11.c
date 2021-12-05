20ab681ec559e22884449d4308e3ab4ffc0a4144
Nyeong <cspfshin@gmail.com>
2021-12-05 22:53:45 +0900

    header file 생성
    file정리하기

---
+++ Section9/9.11/9.11.c
@@ -0,0 +1,10 @@
+#include "my_print_functions.h"
+#include <stdio.h>
+
+int main()
+{
+	print_str("No one lives forever.");
+	printf("I know..");
+
+	return 0;
+}
---
+++ Section9/9.11/my_print_functions.c
@@ -0,0 +1,7 @@
+#include "my_print_functions.h"
+#include <stdio.h>
+
+void print_str(char* str)
+{
+	printf("%s\n", str);
+}
