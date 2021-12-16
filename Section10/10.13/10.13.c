3300c8294fcbd2b3b60a472d42f94639183c5279
Nyeong <cspfshin@gmail.com>
2021-12-17 07:19:29 +0900

    또연습
    한글주석은 깨지는건가..!

--- Section10/10.13/10.13.c
+++ Section10/10.13/10.13.c
@@ -57,7 +57,7 @@ int main()
 	printf("%p\n", parr[0]);
 
 	printf("%p\n", arr);
-	printf("%p\n", arr);
+	printf("%p\n", *arr);
 	printf("%p\n", &arr[0]);
 
 	printf("%p\n", arr[0]);
@@ -65,19 +65,42 @@ int main()
 
 	printf("%d\n", arr[0][0]);
 	printf("%d\n", parr[0][0]);*/
+	
+	// 원하는 값 출력 test
+	//printf("%u\n", (unsigned)**(&arr[0]));
+	//printf("%u\n", (unsigned)**(&arr[0] + 1));
+	//printf("%u\n", (unsigned)*(&arr[1][0]));
+	//printf("%u\n", (unsigned)*(&arr[0][0] + 1));
+	//printf("%u\n", (unsigned)*(&arr[0][1]));
 
-	printf("%p\n", *(arr + 1));
-	printf("%p\n", &arr[1]);
+	//포인터 연산에서 보이는 차이
+	//printf("%u\n", (unsigned)(&arr[0] + 1));
+	//printf("%u\n", (unsigned)(&arr + 1));
+	//printf("%u\n", (unsigned)(arr + 1));
 
-	printf("%p\n", *arr + 2);
-	printf("%p\n", &arr[0][2]);
+	//*,&이해
+	//printf("%u\n", (unsigned)(&arr));
+	//printf("%u\n", (unsigned)(&arr + 1));
+	//printf("%u\n", (unsigned)*(&arr + 1));
+	//printf("%u\n", (unsigned)**(&arr + 1));
+	//printf("%u\n", (unsigned)***(&arr));
 
-	printf("%p\n", *(arr + 1) + 1);
-	printf("%p\n", &arr[1][1]);
+	//printf("%d\n", **(arr + 1)); //indicating arr[1][0]
+	//printf("%d\n", arr[1][0]);
 
-	// quest: print '5' in two ways
-	printf("%d\n", *(*(arr + 1) + 1));
-	printf("%d\n", arr[1][1]);
+	//printf("%p\n", arr + 1);
+	//printf("%p\n", *(arr + 1));
+	//printf("%p\n", &arr[1]);
+
+	//printf("%p\n", *arr + 2);
+	//printf("%p\n", &arr[0][2]);
+
+	//printf("%p\n", *(arr + 1) + 1);
+	//printf("%p\n", &arr[1][1]);
+
+	//// quest: print '5' in two ways
+	//printf("%d\n", *(*(arr + 1) + 1));
+	//printf("%d\n", arr[1][1]);
 
 
 	return 0;
