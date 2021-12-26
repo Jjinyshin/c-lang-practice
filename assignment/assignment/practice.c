fc2ced8bf0ba30a4d9fc4a5966ef27781d0f1d09
Nyeong <cspfshin@gmail.com>
2021-12-26 17:01:14 +0900

    수정
    - 16진수 (10진수표현) : '영문자자체'
    - 줄맞춤, 출력형식 수정

--- assignment/assignment/practice.c
+++ assignment/assignment/practice.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    // 16진수 (10진수표현) : '영문자'
     char c;
     int inter;
 
@@ -9,7 +8,7 @@ int main() {
     for (int i = 0; i <= ('Z' - 'A'); i++)
     {
         inter = 'A' - 'a';
-        printf("%x (%d) : '%c'\t%x (%d) : '%c'\n", c, c, c, c - inter, c - inter, c - inter);
+        printf("%X (%d) : '%c'\t\t%X (%3d) : '%c'\n", c, c, c, c - inter, c - inter, c - inter);
         c++;
     }
     return 0;
