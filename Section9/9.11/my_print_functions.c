b4efbee586c837d0d51400d8ff11251f027c7c84
Nyeong <cspfshin@gmail.com>
2021-12-28 18:46:54 +0900

    Fixed untracked files

--- Section9/9.11/my_print_functions.c
+++
@@ -1,7 +0,0 @@
-#include "my_print_functions.h"
-#include <stdio.h>
-
-void print_str(char* str)
-{
-	printf("%s\n", str);
-}
--- Section9/9.11/my_print_functions.h
+++
@@ -1,5 +0,0 @@
-#pragma once //visual studio에서 사용하는 header guard
-
-void print_str(char* str);
-
-// assume that we have many more longer functions
